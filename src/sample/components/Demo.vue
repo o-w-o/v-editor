9fdb878b753b6789581adbfeceac25f451452cf8
Loong <longyeh@outlook.com>
2017-12-17 22:24:59 +0800

    prettier: 格式化代码

--- src/sample/components/Demo.vue
+++ src/sample/components/Demo.vue
@@ -2,34 +2,40 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <h2>A Vue Editor Component Inspired By StackEdit & Classuer</h2>
-
   </div>
 </template>
 
 <script>
 export default {
   name: 'Demo',
-  data () {
+  data() {
     return {
       msg: 'V-Editor'
     }
+  },
+  created() {
+    console.info('created ...')
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
+h1,
+h2 {
   font-weight: normal;
 }
+
 ul {
   list-style-type: none;
   padding: 0;
 }
+
 li {
   display: inline-block;
   margin: 0 10px;
 }
+
 a {
   color: #42b983;
 }
